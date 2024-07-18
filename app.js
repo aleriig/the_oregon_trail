218b57b33cc1d323eb887328c46583734130dae9
Alejo Rios <alejoriosiglesias@gmail.com>
2024-07-18 19:00:38 +0100

    bug: fix turn sound on/off

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const audio = document.getElementById('backgroundMusic');
 const soundButton = document.getElementById('sound-button');
 
 soundButton.addEventListener('click', () => {
-    audio.play();
-    soundButton.style.display = 'click';
+    if (audio.paused) {
+        audio.play();
+        soundButton.textContent = 'TURN SOUND OFF';
+    } else {
+        audio.pause();
+        soundButton.textContent = 'TURN SOUND ON';
+    }
+    // soundButton.style.display = 'click';
 });
 
-audio.loop = True;
